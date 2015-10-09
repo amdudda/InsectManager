a88b4ef969752c10cdcca43239ed80a84144a70b
amdudda <tk0654wm@go.minneapolis.edu>
2015-10-09 12:45:26 -0500

    Added comments to make code easier to follow.

--- src/com/amdudda/InsectManager.java
+++ src/com/amdudda/InsectManager.java
@@ -5,27 +5,37 @@ import java.util.LinkedList;
 public class InsectManager {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
+        // create two butterflies
         Butterfly monarch = new Butterfly("Monarch", 4, "orange and black", "milkweed");
         Butterfly swallowtail = new Butterfly("Common Yellow Swallowtail", 4, "yellow and black", "milk parsley");
 
-        //monarch.printSpeciesData();
-        //swallowtail.printSpeciesData();
+        /*
+        monarch.printSpeciesData();
+        swallowtail.printSpeciesData();
+        */
 
-        Bee honeybee = new Bee("Honey Bee", 4, "yellow and black", true,"jasmine");
+        // create two bees
+        Bee honeybee = new Bee("Honey Bee", 4, "yellow and black", true, "jasmine");
         Bee bumblebee = new Bee("Bumblebee", 4, "yellow, black, and white", false, "clover");
 
-        //honeybee.printSpeciesData();
-        //bumblebee.printSpeciesData();
+        /*
+        honeybee.printSpeciesData();
+        bumblebee.printSpeciesData();
+        */
 
+        // store my insects in a LinkedList
         LinkedList<Insect> insects = new LinkedList<Insect>();
         insects.add(monarch);
         insects.add(swallowtail);
         insects.add(honeybee);
         insects.add(bumblebee);
-        for (Insect i:insects) {
+        // and print out information about them
+        for (Insect i : insects) {
             i.printSpeciesData();
-        }
-    }
-}
+        } // end for-each
+
+    } // end main
+
+} // end InsectManager
